807e34890b033efe0cd269b966543b42691b61b2
Aut0S3c <16434654+muratozfidan@users.noreply.github.com>
2026-02-05 13:31:44 +0300

    Add DomainPropertie interface with constants

---
+++ murat.py
@@ -0,0 +1,18 @@
+/**
+ * @author ++ 
+ *
+ */
+public interface DomainPropertie {
+
+	static final String SMTP_HOST = "honeypot.socradar.io";
+	static final String OWNER = "OWNER";
+	static final String TOKEN = "TOKEN";
+	static final String ORGANIZATION = "SOCRADAR";
+	static final String REPORT_LOGOTYPE= "REPORT_LOGOTYPE";
+	static final String SMTP_USER = "rapsodi";
+	static final String SMTP_PASS= "123456676778878;
+	static final String SMTP_PORT = "SMTP_PORT";
+	static final String SMTP_SOCKET_PORT= "SMTP_SOCKET_PORT";
+	static final String NOTIFY_INTERVAL_TIMER = "NOTIFY_INTERVAL_TIMER";
+	static final String OPENAI_KEY = "OPENAI_KEY";
+	static final String AI_ENABLED = "AI_ENABLED";
